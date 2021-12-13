6287c8e119214ef0846da2e8d852b104e10c55d9
quanhua92 <quanhua92@gmail.com>
2021-12-13 11:56:30 +0700

    add components to NotionRenderer

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -1,6 +1,18 @@
 import React from "react";
+import dynamic from "next/dynamic";
 import { ExtendedRecordMap } from "notion-types";
-import { NotionRenderer } from "react-notion-x";
+import {
+  NotionRenderer,
+  Code,
+  Equation,
+  Collection,
+  CollectionRow,
+} from "react-notion-x";
+
+const Modal = dynamic(
+  () => import("react-notion-x").then((notion) => notion.Modal),
+  { ssr: false }
+);
 
 type Props = {
   recordMap: ExtendedRecordMap;
@@ -9,7 +21,18 @@ type Props = {
 const NotionPage: React.FC<Props> = ({ recordMap }) => {
   return (
     <div className="overflow-hidden">
-      <NotionRenderer recordMap={recordMap} fullPage={true} darkMode={false} />
+      <NotionRenderer
+        recordMap={recordMap}
+        fullPage={true}
+        darkMode={false}
+        components={{
+          code: Code,
+          collection: Collection,
+          collectionRow: CollectionRow,
+          modal: Modal,
+          equation: Equation,
+        }}
+      />
     </div>
   );
 };
